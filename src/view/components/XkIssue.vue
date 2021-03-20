f0e712923dc482c8e7a65bca824b8c76a9de443e
DreamyTZK <dreamytzk@outlook.com>
2021-03-20 19:36:16 +0800

    🌈style: 添加过度效果

--- src/view/components/XkIssue.vue
+++ src/view/components/XkIssue.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <template v-for="mydata in curSpeakData">
-      <xk-card :speakData="mydata" :key="mydata.number" :name="name" />
-    </template>
-    <button @click="load" v-if="flag">下一页</button>
-    <button v-else>没有更多了</button>
+    <transition-group name="list" tag="p">
+      <template v-for="mydata in speakData">
+        <xk-card :speakData="mydata" :key="mydata.number" :name="name" />
+      </template>
+    </transition-group>
   </div>
 </template>
 <script>
@@ -13,73 +13,20 @@ export default {
   props: ['speakData', 'name'],
   components: { XkCard },
   data() {
-    return {
-      // 记录当前索引
-      cursor: 0,
-      curSpeakData: [],
-      flag: true
-    };
-  },
-  methods: {
-    load() {
-      if (this.curSpeakData.length == this.speakData.length) {
-        this.flag = false;
-        return;
-      }
-      this.curSpeakData = [
-        ...this.curSpeakData,
-        ...this.speakData.slice(
-          this.curSpeakData.length,
-          this.curSpeakData.length + 5
-        )
-      ];
-      if (this.curSpeakData.length == this.speakData.length) {
-        this.flag = false;
-        return;
-      }
-    }
-  },
-  mounted() {
-    // console.log(this.speakData);
-    // this.load();
-  },
-  watch: {
-    speakData(val) {
-      this.load();
-    }
+    return {};
   }
 };
 </script>
 <style scoped>
-@keyframes Gradient {
-  0% {
-    background-position: 0 50%;
-  }
-
-  50% {
-    background-position: 100% 50%;
-  }
-
-  to {
-    background-position: 0 50%;
-  }
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.2s cubic-bezier(0.55, 0.085, 0.68, 0.53);
+  transform-origin: 50% 50%;
 }
-
-button {
-  cursor: pointer;
-  color: #fff;
-  border: 0;
-  margin: 20px auto;
-  border-radius: 0.3125rem;
-  display: block;
-  padding: 0 1rem;
-  height: 40px;
-  font-weight: 500;
-  text-align: center;
-  transition: all 0.5s ease-out;
-  background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
-  background-size: 1000% 1000%;
-  animation: Gradient 90s linear infinite;
-  outline: 0;
+.list-enter,
+.list-leave-to {
+  transform-origin: 50% 50%;
+  transform: scaleY(0) translateZ(0);
+  opacity: 0;
 }
 </style>

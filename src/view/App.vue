c665f190399dc4f9da493fc1a3821c10726e8489
DreamyTZK <dreamytzk@outlook.com>
2021-03-20 19:36:43 +0800

    ⚡️ improvement: 数据获取分批获取，而非全部获取

--- src/view/App.vue
+++ src/view/App.vue
@@ -1,10 +1,15 @@
 <template>
   <div id="ispeak" class="ispeak">
+    <xk-issue :speakData="speakData" :name="name" />
     <div class="loading" v-if="loading">
       <img src="https://7.dusays.com/2021/03/04/d2d5e983e2961.gif" alt="" />
     </div>
-
-    <xk-issue :speakData="speakData" :name="name" />
+    <button @click="getSpeakData(url, next)" v-if="flag && !loading">
+      下一页
+    </button>
+    <div style="text-align: center; margin-top: 20px" v-else>
+      {{ message }}
+    </div>
     <xk-footer />
   </div>
 </template>
@@ -24,33 +29,50 @@ export default {
     return {
       speakData: [],
       loading: true,
-      name: ''
+      // 用户昵称
+      name: '',
+      // 下一个文件名
+      next: '',
+      // 基路径
+      url: '',
+      // 总页数
+      page: 1,
+      flag: true,
+      message: '正在获取，请稍后！'
     };
   },
   methods: {
     // 获得当前Speak数据
-    async getSpeakData() {
-      const { url, name } = Vue.prototype.$speakData;
-      const { data } = await axios.get(url);
-      this.loading = false;
-      this.name = name;
-      if (data.data.length > 0) {
-        this.speakData = data.data;
-      } else {
-        this.speakData = [
-          {
-            number: 1,
-            body: '暂无speak',
-            label: [],
-            updated_at: '2020-11-14T21:44:54+08:00'
+    async getSpeakData(url, path) {
+      if (path) {
+        try {
+          this.loading = true;
+          const { data } = await axios.get(url + path);
+          this.loading = false;
+          if (data.next) {
+            this.next = data.next;
+          } else {
+            this.flag = false;
+            this.message = '没有更多了！';
           }
-        ];
+          this.speakData = this.speakData.concat(data.data);
+        } catch (e) {
+          this.flag = false;
+          this.message = e;
+        }
+      } else {
+        this.flag = false;
+        this.message = '没有更多了！';
       }
+
       // console.log(data);
     }
   },
-  mounted() {
-    this.getSpeakData();
+  async mounted() {
+    const { url, name, file } = Vue.prototype.$speakData;
+    this.name = name;
+    this.url = url;
+    await this.getSpeakData(url, file);
   },
   computed: {},
 
@@ -61,5 +83,36 @@ export default {
 <style scoped>
 .loading {
   text-align: center;
+}
+@keyframes Gradient {
+  0% {
+    background-position: 0 50%;
+  }
+
+  50% {
+    background-position: 100% 50%;
+  }
+
+  to {
+    background-position: 0 50%;
+  }
+}
+
+button {
+  cursor: pointer;
+  color: #fff;
+  border: 0;
+  margin: 20px auto;
+  border-radius: 0.3125rem;
+  display: block;
+  padding: 0 1rem;
+  height: 40px;
+  font-weight: 500;
+  text-align: center;
+  transition: all 0.5s ease-out;
+  background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
+  background-size: 1000% 1000%;
+  animation: Gradient 90s linear infinite;
+  outline: 0;
 }
 </style>
